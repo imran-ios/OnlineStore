b820079d92e91fc607fe023a35011a016c47f7f7
Md Imran Ali test <mdimranali94@gmail.com>
2025-07-11 21:34:20 +0530

    Initial Commit

---
+++ OnlineStore/OnlineStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineStoreApp.swift
+//  OnlineStore
+//
+//  Created by Md Imran Ali on 11/07/25.
+//
+
+import SwiftUI
+
+@main
+struct OnlineStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
